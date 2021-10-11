307d1547c86646788d3cc501145f5055f0f75bbd
Morestel <mystkun6@gmail.com>
2021-10-11 16:22:34 +0200

    init

---
+++ src/main/java/com/quartier/quartiercommunicant/QuartierCommunicantApplication.java
@@ -0,0 +1,13 @@
+package com.quartier.quartiercommunicant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartierCommunicantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuartierCommunicantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quartier/quartiercommunicant/QuartierCommunicantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quartier.quartiercommunicant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartierCommunicantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
