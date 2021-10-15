0ba23c13bf7c02749fe9db0d43a520c713aabdba
MystKun <mystkun6@gmail.com>
2021-10-15 10:18:32 +0200

    suppression import inutile

--- src/main/java/com/quartier/quartiercommunicant/controller/MainController.java
+++ src/main/java/com/quartier/quartiercommunicant/controller/MainController.java
@@ -11,16 +11,11 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
-import java.io.File;
-import java.io.IOException;
 import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.List;
 
 import javax.inject.Inject;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
 
 import com.quartier.quartiercommunicant.model.DemandeStage;
 import com.quartier.quartiercommunicant.model.Fichier;
@@ -33,9 +28,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.w3c.dom.DOMException;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 
 
 @Controller
